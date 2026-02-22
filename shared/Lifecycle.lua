678df897dadb497ddcf52e1f69e6b447d34a79e6
pigxity <87405798+pigxity@users.noreply.github.com>
2026-02-22 12:16:00 -0500

    add Utils, refactor Lifecycle.lua

--- shared/Lifecycle.lua
+++ shared/Lifecycle.lua
@@ -1,31 +1,50 @@
+local class = require(script.Parent.Utils.class)
+local InstanceUtils = require(script.Parent.Utils.InstanceUtils)
+
+
 --@module
 local module = {}
 
 
---@onPostInit
-function module.postInit(manifest)
-    for _, data in pairs(manifest.services) do
+function initService(data, service, ...)
+    if data.kind == "service" then
+        service:_init(...)
+    else
+        class(service)
+        for _, tag in ipairs(data.tags) do
+            InstanceUtils.useCollectionTag(tag, function(inst)
+                service.new(inst)
+            end)
+        end
+    end
+end
+
+
+--@onInit
+function module.initServices(manifest)
+    for _, serviceName in ipairs(manifest.load_order) do
+        local data = manifest.services[serviceName]
         local service = data.getAdornee()
 
         if service._init then
             --build deps list
             if #data.depends.services > 0 or #data.depends.remotes > 0 then
-                local inject_deps = {}
+                local injectDeps = {}
 
                 --service deps
                 for _, dep in ipairs(data.depends.services) do
-                    inject_deps[dep] = manifest.services[dep]
+                    injectDeps[dep] = manifest.services[dep].getAdornee()
                 end
 
                 --remote deps
                 for _, dep in ipairs(data.depends.remotes) do
                     --TODO 
-                    --inject_deps[dep] = 
+                    --injectDeps[dep] = {wrap remote events into a table that is similar to a service}
                 end
 
-                service.init(inject_deps)
+                initService(data, service, injectDeps)
             else
-                service._init()
+                initService(data, service)
             end
         end
     end
--- shared/Utils/class.lua
+++ shared/Utils/class.lua
@@ -37,7 +37,7 @@ type function getFields(T: type)
     return params.head[1]
 end
 
---@indexed
+--@indexed, Utils
 local function class<T>(class: T)
     class.__index = class 
 
